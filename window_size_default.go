73b6cb87e5ff75ff71dc9e1b1dfdae3af9c0b802
ashokhilo <ashok@hiloailabs.com>
2026-09-03 20:16:48 +0530

    minor bug fix

--- window_size_default.go
+++ window_size_default.go
@@ -2,6 +2,8 @@
 
 package main
 
+// initialWindowSize returns the default starting width and height for the application window.
+// This is used as a fallback for operating systems other than macOS (darwin) and Windows.
 func initialWindowSize() (int, int) {
 	return 1200, 800
 }
